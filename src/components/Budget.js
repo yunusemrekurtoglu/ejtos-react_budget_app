8a1dac440ce988fadb98c2fa8d6e9680eaa614cf
YunusEmreKurtoglu <yunusemrekurtoglu@gmail.com>
2024-05-02 22:34:21 -0400

    Fixed the issue with state.budget not updating by dispatching from budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,17 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
+    const { dispatch } = useContext(AppContext);
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value
+        });     
     }
-    return (
+    return ( 
 <div className='alert alert-secondary'>
 <span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
