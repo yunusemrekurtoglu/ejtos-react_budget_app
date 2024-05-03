03fc9db3c5c68120b38e9d32b1e00317d1bf2789
YunusEmreKurtoglu <yunusemrekurtoglu@gmail.com>
2024-05-03 14:29:55 -0400

    done up till currency selection component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,19 +1,29 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+//import ExpenseTotal from './ExpenseTotal';
 const Budget = () => {
     const { dispatch } = useContext(AppContext);
     const { budget } = useContext(AppContext);
-    const { totalExpenses } = useContext(AppContext);
+        
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+    }, 0);
+
     const [newBudget, setNewBudget] = useState(budget);
-    
     const handleBudgetChange = (event) => {
         if(event.target.value > 20000) {
             alert("The value cannot exceed 20000 £");
             // may need to revert to former budget value here
             return;
         }
+        if(event.target.value < 0) {
+            alert("The budget cannot be negative");
+            return;
+        }
+                
         if(event.target.value < totalExpenses) {
-            alert("You cannot reduce budget value lower than thhe spending");
+            alert("You cannot reduce budget value lower than the spending");
             return;
         }
         setNewBudget(event.target.value);
